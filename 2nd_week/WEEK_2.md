fc1b61b296d9da25b635b1f75ba0649742aefedc
WooYoo <puba5@naver.com>
2020-08-09 13:13:44 +0900

    feat : 리드미 수정, 파이썬 파일 경로 수정

---
+++ 2nd_week/WEEK_2.md
@@ -0,0 +1,51 @@
+# Relay08
+
+## 2nd Week
+
+- 참가자
+
+  - J015*권순주
+    J016*권영길
+    J053*김종은
+    J054*김진관
+    J089*박지홍
+    J090*박진영
+    J126*유진우
+    J127*유현우
+    J164*이청명
+    J165*이한주
+    J202*차효준
+    J203*최기환
+
+- **Front-end**
+
+  1. 자연어 처리된 태그 값을 이용하여 `사연게시판 카테고리화`
+  1. `카테고리별 필터링`
+
+     ![사연게시판](https://github.com/boostcamp-2020/relay_08/raw/master/%EA%B8%B0%ED%9A%8D%EC%84%9C/img/board.png)
+
+  - `React` 기반 (npm install -> npm run start)
+  - 자연어 처리된 `.csv 파일`을 읽어와 리스트화
+    (\*server 및 back-end 미존재)
+  - `papaparse` 이용 .csv 로딩
+
+## Python Docker 설정 부분
+
+### pull&run image
+
+docker pull chahtk/aipart
+docker run -it -e LC_ALL=C.UTF-8 --rm chahtk/aipart bash
+
+### in docker
+
+cd gitworks
+cd Mecag-ko-for-Google-Colab
+python3.7 nlp.py
+
+## 프론트 실행 부분
+
+```
+npm install 으로 노드 패키지 모듈 설치
+npm run start 로 실행
+
+```
---
+++ 2nd_week/src/nlp.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import re
+from krwordrank.word import KRWordRank
+
+sentence_pattern = re.compile('\n+|[.?!]')
+
+df = pd.read_csv('./contents.csv')
+data = df[['title', 'body']].agg('\n'.join, axis=1)
+split_data = [sentence_pattern.split(row) for row in data]
+
+min_count = 4   # 단어의 최소 출현 빈도수 (그래프 생성 시)
+max_length = 10 # 단어의 최대 길이
+wordrank_extractor = KRWordRank(min_count=min_count, max_length=max_length)
+
+beta = 0.85    # PageRank의 decaying factor beta
+max_iter = 10
+verbose = True
+
+df.tagList = df.tagList.astype(str)
+for i, row in enumerate(split_data):
+    try:
+        keywords, rank, graph = wordrank_extractor.extract(row, beta, max_iter)
+        print(f'[success] index: {i}, len: {len(row)}, keywords: {tuple(keywords.keys())}')
+        df._set_value(i, 'tagList', ' '.join(list(keywords.keys())))
+    except:
+        print(f'[fail] index: {i}, len: {len(row)}')
+        df._set_value(i, 'tagList', '')
+
+df.to_csv('./tag_contents.csv', index=False)
