e6af6465ccd33af15bd0b3285d59cadb98a47ce1
WooYoo <puba5@naver.com>
2020-08-09 13:24:32 +0900

    feat : csv 파일 추가 및 인코딩 문제 해결
    인코딩 문제로 잘못된 문자 삭제

--- 2nd_week/src/components/views/List/List.js
+++ 2nd_week/src/components/views/List/List.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import Post from "./Sections/Post";
 import Tag from "./Sections/Tag";
 import Papa from "papaparse";
-​
+
 const postList = [
   {
     name: "유진우",
@@ -32,27 +32,27 @@ const postList = [
     tagList: ["안녕", "안냥"],
   },
 ];
-​
+
 const MAX_LENGTH = 10;
-​
+
 export default function List() {
   const [tagFilter, setTagFilter] = useState(null);
   // csv -> json or array로 변환한 파일을 저장할 state
   const [data, setData] = useState([]);
-​
+
   // 배열을 오브젝트로 변경
   const arrToObj = (arr) =>
     arr
       .map((element, index, ar) => {
-        if (index != 0 && index != ar.length -1)
+        if (index != 0 && index != ar.length - 1)
           return new Object({
             name: element[1],
             content: element[2],
-            tagList: element[3] ? element[3].split(/\s/) : []
+            tagList: element[3] ? element[3].split(/\s/) : [],
           });
       })
       .splice(1, arr.length - 2);
-​
+
   // csv를 object 파일로 변환
   const fetchCsv = () => {
     return fetch("/tag_contents.csv").then(function (response) {
@@ -63,23 +63,23 @@ export default function List() {
       });
     });
   };
-​
+
   useEffect(() => {
     getCsvData();
   }, []);
-​
+
   const getData = (result) => {
     console.log(arrToObj(result.data));
     setData(arrToObj(result.data));
   };
-​
+
   const getCsvData = async () => {
     let csvData = await fetchCsv();
     Papa.parse(csvData, {
       complete: getData,
     });
   };
-​
+
   const tagListAll = (postList) => {
     let notDupTag = new Set();
     postList.map((post) => {
@@ -89,9 +89,9 @@ export default function List() {
     });
     return [...notDupTag];
   };
-​
+
   // 태그에 숫자를 부여
-​
+
   const addTagNumber = (os) => {
     let tagMap = new Map();
     let cnt = 0;
@@ -108,9 +108,9 @@ export default function List() {
     }
     return os;
   };
-​
+
   console.log(addTagNumber(data));
-​
+
   return (
     <Wrapper>
       <Title>사연게시판</Title>
@@ -132,30 +132,30 @@ export default function List() {
     </Wrapper>
   );
 }
-​
+
 const Wrapper = styled.div`
   padding-top: 5%;
   padding-left: 30%;
   padding-right: 30%;
 `;
-​
+
 const Content = styled.div`
   display: flex;
 `;
-​
+
 const PostList = styled.div`
   width: 60%;
   border: 2px black solid;
   padding: 0 10px 10px 10px;
 `;
-​
+
 const TagList = styled.div`
   padding: 15px;
   border: 2px black solid;
   width: 30%;
   margin-left: 10px;
 `;
-​
+
 const Title = styled.div`
   text-align: center;
   font-size: 50px;
