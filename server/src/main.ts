adf28466863ef14091cf8eaf8e6b476124cd7d1e
youngxpepp <youngxpepp@gmail.com>
2020-08-14 17:43:41 +0900

    feat: swagger-api 환경 설정

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "@src/AppModule";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 import type { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+    const options = new DocumentBuilder().setTitle("relay_08").build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup("swagger-api", app, document);
+
     await app.listen(3000);
 }
 
